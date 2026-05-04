15be6f2a71b8d4cc661f6dc5a84e9dca6ff62dbf
Abbas Abdul <aa9059476@gmail.com>
2026-05-04 14:44:18 +0530

    Add files via upload

---
+++ run time input.py
@@ -0,0 +1,97 @@
+'''a=10
+b=20
+print(a+b)'''
+
+'''a=2
+b=5
+c=a+b
+print(c)'''
+#print(5+9)
+#run_time input()
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("1name"))
+print((a+" "+b).title())'''
+
+'''city=input("city name")
+print("city is",city)'''
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+           
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("enter the c value"))
+print(a+b+c)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a,b)'''
+
+'''a=10
+b=20
+a,b=10,20
+a,b=b,a
+print(b,a)'''
+
+'''a=10
+b=20
+c=a
+a=b
+b=c
+print(a,b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a,b)''' 
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+'''a=input("data1")
+b=input("data2")
+temp=a
+a=b
+b=temp
+print("after swapping a=%s,b=%s" %(a,b))'''
+
+
+
+
+
+
+                 
+                 
