627a4cf2a6e5264a08c28d72213cd7376083ec40
Abbas Abdul <aa9059476@gmail.com>
2026-05-05 14:34:28 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,113 @@
+#if-condition by using logical operators
+#and,or,not
+'''a=7
+b=9
+if a<b and b>a:
+    print("true")'''
+
+'''a=11
+b=13
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=5
+b=10
+if a!=b and b==a:
+    print("true")'''
+
+'''a=9
+b=12
+if a<b or b>a:
+    print("true")'''
+
+'''a=11
+b=13
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=7
+b=9
+if a!=b or b==a:
+    print("true")'''
+
+'''a=4
+b=8
+if not a<b:
+    print("less")'''
+
+'''a=4
+b=8
+if not a>b:
+    print("less")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b and b>a:
+    print("less")'''
+
+'''a=input("data")
+if a=="abbas" or a=="abdul":
+    print("true")'''
+
+#if-condition by using identify operators
+#is,is not
+'''a=6
+if type(a) is int:
+    print("it is int")'''
+
+'''a=6
+if type(a) is not int:
+    print("it is int")'''
+
+'''a=int(input("a value"))
+if type(a) is input:
+    print("true")'''
+
+#if-condition by using membership operators
+#in,not in
+'''a=[10,20,30,40,50,60]
+if 60 in a:
+    print("true")'''
+
+'''a=[10,20,30,40,50,60]
+if 60 not in a:
+    print("true")'''
+
+'''a=[10,20,30,40,50,60]
+if 70 not in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 40 in a:
+    print("true")'''
+
+'''a=["c","c++","java","python"]
+b=input("data")
+if b in a:
+    print("true")'''
+
+#if-else conditions by using comparision operators
+'''a=3
+b=6
+if a<b:
+    print("less")
+else:
+    print("true")'''
+        
+
+'''a=3
+b=6
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+
+
+
+
+
+            
+
+
+
