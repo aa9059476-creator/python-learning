fd16556fc2e66c67d5176cc20e3863e87dd52893
Abbas Abdul <aa9059476@gmail.com>
2026-05-06 20:45:32 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,203 @@
+#if-elif-else
+'''a=4
+b=8
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=4
+b=8
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=4
+b=8
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=4
+b=9
+if a>b and b>a:
+    print("less")
+elif a<b or b<a:
+    print("greater")
+elif not b>a:
+    print("not equal")
+else:
+    print("true")'''
+
+'''a=[2,3,4,5,6]
+if 3 in a:
+    print("true")
+else 7 in a:
+    print("false")'''
+
+'''a=[7,8,9,10,11]
+if 13 not in a:
+    print("true")
+if 8 not in a:
+    print("false")'''
+
+'''a=5
+if type a is int:
+    print("true")
+elif type a is int:
+    print("false")'''
+'''a=6
+if type a is not int:
+    print("less")
+elif type a is not int:
+    print("true")'''
+
+#multiple-if
+'''a=8
+b=10
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=8
+b=10
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=7
+b=9
+if a<b and b<a:
+    print("less")
+if a<b or b<a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=7
+if type a is int:
+    print("true")
+if type a is not int:
+    print("less")'''
+
+'''a=[1,2,3,4,5]
+if 2 in a:
+    print("true")
+if 3 is not in:
+    print("true")'''
+
+#nested-if conditions
+'''a=5
+b=10
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=5
+b=10
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=5
+b=10
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")'''
+
+'''a=5
+b=10
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+        else:
+            print("true")'''
+
+'''a=9
+b=11
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("true")'''
+
+'''a=5
+b=10
+if a>b:
+    print("less")
+          if b==a:
+          print("greater")
+          else:
+              print("true")
+else:
+    print("false")'''
+
+'''a=5
+b=10
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+    elif a!=b:
+        print("not equal")'''
+
+#voting
+'''age=int(input("enter the age"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+#evan or odd
+'''num=int(input("enter the number"))
+if num%2==0:
+    print("it is even")
+else:
+    print("it is odd")'''
+
+#vowels
+vowels=["a","e","i","O","u"]
+'''a=input("enter the letter").lower()
+if a in vowels:
+    print("it is vowel")
+else:
+    print("it is consonant')'''
+
+#guest code
+'''name=input("enter the name").lower()
+if name=="abbas":
+    print("welcome',name)
+else:
+    print("welcome guest")'''
+
+
+            
+    
+
+
+
+
+    
+        
+        
+        
+        
