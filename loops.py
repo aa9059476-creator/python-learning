eb4fc96e77a8868146658f2bde35aa5a1630b362
Abbas Abdul <aa9059476@gmail.com>
2026-05-07 20:27:44 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,64 @@
+#loops
+#for,while,range,break,continue,pass
+#for loop
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[10,30,40,70,90]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[3,4,5,6,7,8]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''d={"year":2026,"month":5,"date":7}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.values():
+    rint(i)
+    print(type(d))
+    print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i,end=" ")
+    print(type(a))
+    print(type(i))'''
+
+'''a=["apple","banana","mango"]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[3,5.6,"abbas",8+9j,True,False]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=["codegnan","pyton","course"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+
+    
