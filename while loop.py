43d0154a64d8524e50a8c4e757e48ed7d8fcdcdc
Abbas Abdul <aa9059476@gmail.com>
2026-05-08 20:58:28 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,129 @@
+#while loop
+'''a=10
+while a<1:
+    print("true")'''
+'''a=10
+while a<1:
+    print("true")'''
+
+'''a=20
+while a>=1:
+    print(a)
+    a=a-1'''
+'''a=10
+while a>=1:
+    print(a    a+=1'''
+'''a=1
+while a<10:
+    print(a)
+    a+=1'''
+'''a=10
+while a>1:
+    print(a)
+    a-=1'''
+#voting
+'''while True:
+    age=int(input("enter the age"))
+    if age>=18:
+        print("eligible for vote")
+    else:
+        print("not eligible")'''
+
+#range()
+#start-stop-step
+'''for i in range(10):
+    print(i)'''
+
+'''for i in range(1,16):
+    print(i)'''
+
+'''for i in range(5,26):
+    print(i)'''
+
+'''for i in range(2,20,2):
+    print(i)'''
+
+'''for i in range(5,50,5):
+    print(i)'''
+
+#student marks
+'''while True:
+    marks=int(input("enter student marks"))
+    if marks in range(91,101):
+        print("Grade-A")
+    elif marks in range(81,91):
+        print("Grade-B")
+    elif marks in range(71,81):
+        print("Grade-C")
+    elif marks in range(50,71):
+        print("Grade-D")
+    else:
+        print("Fail")'''
+
+#break
+'''a=30
+while a>5:
+    a=a-1
+    if a==15:
+        break'''
+
+'''a=30
+while a>5:
+    a=a-1
+    if a==15:
+        break
+    print(a)'''
+
+'''for i in range(19):
+    if i==10:
+        break
+    print(i)'''
+
+'''a="python"
+for i in a:
+    if i=='o':
+        break
+    print(i)'''
+
+#continue
+'''a=20
+while a>1:
+    print(a)
+    a=a-1
+    if a==10:
+        continue'''
+
+'''a=20
+while a>1:
+    a=a-1
+    if a==10:
+        continue
+    print(a)'''
+
+'''for i in range(15):
+    if i==12:
+        continue
+    print(i)'''
+
+a="python"
+'''for i in a:
+    if i=="t':
+    continue
+print(i)'''
+
+#pass
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==5:
+        pass'''
+
+'''for i in range(5):
+    if i==2:
+        pass
+    print(i)'''
+
+
+        
+
