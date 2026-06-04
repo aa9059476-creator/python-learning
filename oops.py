20be71e2c620205271ee5c17ba46aafc5f7db986
Abbas Abdul <aa9059476@gmail.com>
2026-06-04 09:54:13 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,36 @@
+#oops
+'''class classname():
+    name="codegnan"
+    year=2018
+    city="vja"
+    def fname(method_name):
+        print("statements............")
+a=classname()
+a.fname()'''
+
+#class declaratiton
+'''class Details():
+    name="abbas"
+    age=24
+    place="vja"
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.display()'''
+
+#object instantiation
+'''class Details():
+    def Data(self,name,age,place):
+        self,name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details()
+print(dir(a))
+a.Data("abbas",24,"vja")
+a.display()
+b=Details()
+b.data("abdul",25,"vja")
+b.display()'''
